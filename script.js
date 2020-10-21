7767710ace56113e053cfc6a328266a69d59b887
Dmitry Tkach <kathudawar@gmail.com>
2020-10-21 09:28:57 +0300

    Third commit

--- script.js
+++ script.js
@@ -6,7 +6,29 @@ const todoList = document.querySelector('.list');
 const createTask = (value) => {
     const task = document.createElement('div');
     task.textContent = value;
+// add .task, .unsuccess
+    task.classList.add('task', 'unsuccess');
+// add checkbox
+    const checkbox = document.createElement('input');
+    checkbox.setAttribute('type', 'checkbox');
+    checkbox.classList.add('status');
+    task.appendChild(checkbox);
+    //finish add checkbox
+    checkbox.addEventListener('click', completeStatus);
     return task;
+}
+
+const completeStatus = (event) => {
+    const clickedCheckbox = event.target;
+    const checkboxParent = clickedCheckbox.parentElement;
+
+    if (!!clickedCheckbox.checked) {
+        checkboxParent.classList.remove('unsuccess');
+        checkboxParent.classList.add('success');
+    } else {
+        checkboxParent.classList.remove('success');
+        checkboxParent.classList.add('unsuccess');
+    }
 }
 
 const addTask = () => {
